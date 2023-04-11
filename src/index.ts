a37846bfe02bb97bdf8ff7047c612ddc6cabe410
Roie Schwaber-Cohen <roie.cohen@gmail.com>
2023-04-11 12:03:50 -0700

    cleanup

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,22 @@
 import { BabyAGI } from './babyAgi/babyAgi'
 import { PineconeStore } from 'langchain/vectorstores';
 import { OpenAIEmbeddings } from 'langchain/embeddings/openai';
-import { getPineconeClient } from './utils/pinecone';
+import { getPineconeClient, waitUntilIndexIsReady } from './utils/pinecone';
 import { OpenAI } from 'langchain/llms/openai';
 
 const run = async () => {
   const client = await getPineconeClient()
+  const indexName = "babyagi"
+
+  await client.createIndex({
+    createRequest: {
+      name: "babyagi",
+      dimension: 1536,
+    }
+  })
+
+  await waitUntilIndexIsReady(client, indexName)
+
   const pineconeIndex = client.Index("babyagi")
 
   const model = new OpenAI({ temperature: 0 });
@@ -19,7 +30,11 @@ const run = async () => {
 
   const babyAgi = new BabyAGI(model, false, vectorStore, 10);
 
-  babyAgi.execute({ "objective": "Create a weather report for SF" })
+  await babyAgi.execute({ "objective": "Create a weather report for SF" })
+
+  //Cleanup
+  client.deleteIndex({ indexName })
+
 }
 
 run()
